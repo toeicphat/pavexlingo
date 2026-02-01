d62171dccbc19ec514682100a7916487ca1dd785
Nguyễn Minh QUân <minhquan28032004@gmail.com>
2026-02-01 10:05:05 +0700

    fixx

--- services/readingLibrary.ts
+++ services/readingLibrary.ts
@@ -1,16 +1,7 @@
 
 import { ReadingTestData } from '../types';
 
-export interface ReadingTestSet {
-    id: number;
-    title: string;
-    description: string;
-    parts: {
-        part5: ReadingTestData;
-        part6: ReadingTestData;
-        part7: ReadingTestData;
-    }
-}
+
 
 // Data has been cleared as requested
 export const allReadingTests: ReadingTestSet[] = [];
